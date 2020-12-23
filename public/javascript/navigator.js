bc212b3bb06be11642f1cfeb35fa57c2cc1bca99
Aayush Chhabra <aayushgen@gmail.com>
2020-12-22 22:19:51 -0800

    update

--- public/javascript/navigator.js
+++ public/javascript/navigator.js
@@ -1,5 +1,9 @@
 const vibrate = document.querySelector('button')
 
+const vib = () => {
+    window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]);
+}
+
 vibrate.addEventListener('click', () => {
-  let stat = window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]);
+    setInterval(vib, 100);
 });
